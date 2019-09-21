112fbe19b7f2e3e7fd508c34b96314be35f0e15c
Patrick McMahon <pmcmahon14@gmail.com>
2019-09-21 10:54:59 -0700

    styled view as a card container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>Game screen</Text>
+            <Text style={styles.title}>Let's Play!</Text>
             <View style={styles.inputContainer}>
                 <Text>Please Choose a Number</Text>
                 <TextInput/>
@@ -30,7 +30,18 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 5,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
     },
     buttonContainer: {
         flexDirection: 'row',
